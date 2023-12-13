b5c2d7c222146f005b05824cb8012abbd2ca3932
Bryan Libod-Libod <bryan@invedent.com>
2023-12-14 01:19:56 +0800

    added image

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -15,13 +15,18 @@ interface PersonProps {
 const Person = ({ person, handleShowAlert }: PersonProps) => {
   return (
     <IonItemSliding>
-      <IonItem>
-        <IonLabel>
-          <h4>
-            {person.name.first} {person.name.last}
-          </h4>
-          <span className="email">{person.email}</span>
-        </IonLabel>
+      <IonItem className="myList">
+        <div className="person-container">
+          <img src={person.picture.large} alt={person.name.first} />
+          <div>
+            <IonLabel>
+              <h4>
+                {person.name.first} {person.name.last}
+              </h4>
+              <span className="email">{person.email}</span>
+            </IonLabel>
+          </div>
+        </div>
       </IonItem>
       <IonItemOptions>
         <IonItemOption
